bde683a53fb1faf195258bcb9c2145ec513aa3f3
joel <jlmukono@gamil.com>
2019-02-15 14:07:05 +0300

    add else if condiotion so zeros cannot be used to create the triangle

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,15 @@ function triangleTracker(length,width,height){
 
       if(length==width && width==height && height==length ){
              alert(triangleMessages[0]);
-           }
+      }
+
+      else if(length == 0 || width==0 || height==0){
+             alert(triangleMessages[3]);
+      }
+
       else{
             alert(triangleMessages[3]);
       }
 
 }
-triangleTracker(2,2,2);
+triangleTracker(0,2,2);
