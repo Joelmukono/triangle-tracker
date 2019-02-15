92f654c5f82a080188bf12962e6b7c0b109196f0
joel <jlmukono@gamil.com>
2019-02-15 15:10:55 +0300

    remove add function and add varriables previously intended for comparison

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
-function add(number1, number2){
- return number1+number2;
-}
+
 
 function triangleTracker(length,width,height){
-      var heightCompare= add(height,width);
-      var lengthCompare = add(width,length);
-      var widthCompare = add(length,height);
 
       var triangleMessages=["Equilateral triangle","isosceles triangle","scalene triangle","not triangle"];
 
-      if(length==width && width==height && height==length ){
+      if(length == width && width == height && height == length ){
              alert(triangleMessages[0]);
       }
 
-      else if(length == 0 || width==0 || height==0){
+      else if(length == 0 || width == 0 || height == 0){
              alert(triangleMessages[3]);
       }
 
-      else if(length==width || width==height || height==length){
+      else if(length == width || width == height || height == length){
              alert(triangleMessages[1]);
       }
 
-      else if(length!= width || width!= height || height!= length ){
+      else if(length != width || width != height || height != length ){
              alert(triangleMessages[2]);
       }
 
