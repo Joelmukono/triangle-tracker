e44c04eb0ded0ff524b3a94449352caa73c708c5
joel <jlmukono@gamil.com>
2019-02-15 13:47:05 +0300

    declare first if else condition to check if dimensions form an equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,9 @@ function add(number1, number2){
 function triangleTracker(length,width,height){
       var triangleMessages=['"Equilateral triangle","isosceles triangle","scalene triangle","not triangle"'];
 
+      if(length==width && width==height && height==length ){
+             alert(triangleNames[0]);
+           }
+
 }
+triangleTracker(2,2,2);
