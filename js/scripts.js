83bdf31b03d24a16b1ea7d42ebeb9d2dabf81ae3
joel <jlmukono@gamil.com>
2019-02-15 17:14:22 +0300

    fix bugs and add output to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,39 @@
 
+function triangleTracker(){
 
-
-function triangleTracker(length,width,height){
-      var height = getElementById('height1');
-      var width = getElementById('width1');
-      var lenght = getElementById('lenght1');
+      var height = document.getElementById('height1').value;
+      var width = document.getElementById('width1').value;
+      var lenght = document.getElementById('length1').value;
 
       var height2 = parseInt(height);
       var width2 = parseInt(width);
       var lenght2 = parseInt(lenght);
 
-      var triangleMessages=["Equilateral triangle","isosceles triangle","scalene triangle","not triangle"];
+      var triangleMessages=["The triangle is an Equilateral triangle","The isosceles triangle","scalene triangle","not triangle"];
 
-
+      //check if sides are correct
       if(height2 + width2 < lenght2 || width2 + lenght2 < height2 || lenght2 + height2 < width2 ){
-             alert(triangleMessages[3]);
+             document.getElementById('showResult').innerHTML= triangleMessages[3];
       }
+      //check if sides are Equilateral
       else if(lenght2 == width2 && width2 == height2 && height2 == lenght2 ){
-             alert(triangleMessages[0]);
+             document.getElementById('showResult').innerHTML= triangleMessages[0];
       }
-
+      //check if sides are zero
       else if(lenght2 == 0 || width2 == 0 || height2 == 0){
-             alert(triangleMessages[3]);
+             document.getElementById('showResult').innerHTML = triangleMessages[3];
       }
-
+      //check if sides are isosceles
       else if(lenght2 == width2 || width2 == height2 || height2 == lenght2){
-             alert(triangleMessages[1]);
+             document.getElementById('showResult').innerHTML = triangleMessages[1];
       }
-
-      else if(lenght2 != width2 || width2 != height2 || height2 != lenght2 ){
-             alert(triangleMessages[2]);
+      //check if sides are scalene
+      else if(lenght2 != width2 && width2 != height2 && height2 != lenght2 && height2 + width2 > lenght2 && width2 + lenght2 > height2 && lenght2 + height2 > width2){
+             document.getElementById('showResult').innerHTML = triangleMessages[2];
       }
-
+      // not triangle
       else{
-            alert(triangleMessages[3]);
+            document.getElementById('showResult').innerHTML = triangleMessages[3];
       }
 
 }
