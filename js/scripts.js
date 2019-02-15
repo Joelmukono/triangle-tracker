24c4ef19e7ba4937590ab5f9e9952b08eb7da73d
joel <jlmukono@gamil.com>
2019-02-15 15:50:14 +0300

    add input boxes to html file used for taking input from user

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 
 
+
 function triangleTracker(length,width,height){
+      var height = getElementById('height1').value();
+      var width = getElementById('width1').value();
+      var length = getElementById('width1').value();
+
+      var height1 = parseInt(height);
+      var width1 = parseInt(width);
+      var length1 = parseInt(length);
 
       var triangleMessages=["Equilateral triangle","isosceles triangle","scalene triangle","not triangle"];
 
@@ -29,4 +37,3 @@ function triangleTracker(length,width,height){
       }
 
 }
-triangleTracker(9,4,7);
