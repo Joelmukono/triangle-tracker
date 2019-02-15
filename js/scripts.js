6ff8364835b295744bcfdcd6c9f1756b8c15c380
joel <jlmukono@gamil.com>
2019-02-15 16:11:07 +0300

    add new varriable to parse text input to numbers and change varriables in conditions to match the parse int varriables

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,29 @@ function triangleTracker(length,width,height){
       var width = getElementById('width1');
       var lenght = getElementById('lenght1');
 
+      var height2 = parseInt(height);
+      var width2 = parseInt(width);
+      var lenght2 = parseInt(lenght);
+
       var triangleMessages=["Equilateral triangle","isosceles triangle","scalene triangle","not triangle"];
 
 
-      if(height+width<length || width+length< height || length + height < width ){
+      if(height2 + width2 < lenght2 || width2 + lenght2 < height2 || lenght2 + height2 < width2 ){
              alert(triangleMessages[3]);
       }
-      else if(length == width && width == height && height == length ){
+      else if(lenght2 == width2 && width2 == height2 && height2 == lenght2 ){
              alert(triangleMessages[0]);
       }
 
-      else if(length == 0 || width == 0 || height == 0){
+      else if(lenght2 == 0 || width2 == 0 || height2 == 0){
              alert(triangleMessages[3]);
       }
 
-      else if(length == width || width == height || height == length){
+      else if(lenght2 == width2 || width2 == height2 || height2 == lenght2){
              alert(triangleMessages[1]);
       }
 
-      else if(length != width || width != height || height != length ){
+      else if(lenght2 != width2 || width2 != height2 || height2 != lenght2 ){
              alert(triangleMessages[2]);
       }
 
