9551c19057a6d82baf16ac340d1f7799aec68b5b
joel <jlmukono@gamil.com>
2019-02-17 14:47:29 +0300

    add background div

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function triangleTracker(){
       var width2 = parseInt(width);
       var lenght2 = parseInt(lenght);
 
-      var triangleMessages=["The triangle dimensions from an Equilateral triangle","The triangle dimensions from an isosceles triangle","The triangle dimensions from a scalene triangle","The triangle dimensions, don't from a triangle"];
+      var triangleMessages=["The triangle dimensions from an EQUILATERAL TRIANGLE","The triangle dimensions from an ISOSCELES TRIANGLE","The triangle dimensions from a SCALENE TRIANGLE","The triangle dimensions, don't from a triangle"];
 
       //check if sides are correct
       if(height2 + width2 < lenght2 || width2 + lenght2 < height2 || lenght2 + height2 < width2 ){
